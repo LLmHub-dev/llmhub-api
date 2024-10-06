6f320d21a50448d0133c81fd23e5c4b5dabe0930
PrateekJannu <prateekjannu@gmail.com>
2024-10-06 16:14:15 -0400

    api_key auth basic

--- FastAPI_Dir/__init__.py
+++ FastAPI_Dir/__init__.py
@@ -1,17 +1,33 @@
 import azure.functions as func
-
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import FastAPI, Depends, HTTPException, Security
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from starlette.status import HTTP_403_FORBIDDEN
 import fastapi
 
-app = fastapi.FastAPI()
+API_KEY = "llmhub.dev"  # Replace with your actual API key
+security = HTTPBearer()
 
-@app.get("/api")
+
+def verify_api_key(credentials: HTTPAuthorizationCredentials = Security(security)):
+    if credentials.credentials != API_KEY:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN,
+            detail="Invalid API Key",
+        )
+
+
+app = fastapi.FastAPI(dependencies=[Depends(verify_api_key)])
+
+
+@app.get("/v1/chat")
 async def index():
     return {
         "info": "Try /hello/Prateek for parameterized route.",
     }
 
 
-@app.get("/hello/{name}")
+@app.get("/v1/hello/{name}")
 async def get_name(name: str):
     return {
         "name": name,
