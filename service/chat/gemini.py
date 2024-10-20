243c8b0998eedaf7285b4ed5613477aefe7c5086
PrateekJannu <prateekjannu@gmail.com>
2024-10-20 19:37:27 -0400

    Fixed context history

--- service/chat/gemini.py
+++ service/chat/gemini.py
@@ -11,7 +11,7 @@ from pydantic_types.chat import (
 def Gemini_Chat_Completions(request):
     genai.configure(api_key=os.environ["GEMINI_API_KEY"])
     model = genai.GenerativeModel("gemini-1.5-flash")
-    chat_history = [{"role": msg.role, "parts": msg.content} for msg in request.messages]
+    chat_history = [{"role": "model" if msg.role=="assistant" else "user" , "parts": msg.content} for msg in request.messages]
 
     chat = model.start_chat(
         history=chat_history,
